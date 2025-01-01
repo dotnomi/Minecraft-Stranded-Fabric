6eaff686a2970ac9f4ef8e542e2c3d40b09ebfd1
DotNomi <dommiii125@gmail.com>
2025-01-01 18:02:48 +0100

    Added data generation

--- src/main/java/com/dotnomi/stranded/StrandedDataGenerator.java
+++ src/main/java/com/dotnomi/stranded/StrandedDataGenerator.java
@@ -1,11 +1,24 @@
 package com.dotnomi.stranded;
 
+import com.dotnomi.stranded.datagen.ModBlockTagProvider;
+import com.dotnomi.stranded.datagen.ModItemTagProvider;
+import com.dotnomi.stranded.datagen.ModLootTableProvider;
+import com.dotnomi.stranded.datagen.ModModelProvider;
+import com.dotnomi.stranded.datagen.ModRecipeProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class StrandedDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		Stranded.LOGGER.debug("Stranded Mod data generation...");
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
+		Stranded.LOGGER.debug("Stranded Mod data generated successfully");
 	}
 }
---
+++ src/main/java/com/dotnomi/stranded/datagen/ModRecipeProvider.java
@@ -0,0 +1,31 @@
+package com.dotnomi.stranded.datagen;
+
+import com.dotnomi.stranded.Stranded;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.recipe.RecipeExporter;
+import net.minecraft.data.recipe.RecipeGenerator;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeProvider extends FabricRecipeProvider {
+	public ModRecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+		super(output, registriesFuture);
+	}
+
+	@Override
+	protected RecipeGenerator getRecipeGenerator(RegistryWrapper.WrapperLookup wrapperLookup, RecipeExporter recipeExporter) {
+		return new RecipeGenerator(wrapperLookup, recipeExporter) {
+			@Override
+			public void generate() {
+
+			}
+		};
+	}
+
+	@Override
+	public String getName() {
+		return Stranded.MOD_ID;
+	}
+}
